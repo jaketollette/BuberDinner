f7f4384d55d97f252e533dc74e224d486f206d03
Jacob Tollette <jaketollette@gmail.com>
2024-11-28 09:44:40 -0600

    Add validators

--- BuberDinner.Api/Controllers/ApiController.cs
+++ BuberDinner.Api/Controllers/ApiController.cs
@@ -1,31 +1,55 @@
 using BuberDinner.Api.Common.Http;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberDinner.Api.Controllers
 {
     [ApiController]
     public class ApiController : ControllerBase
     {
-      protected IActionResult Problem(List<Error> errors)
-      {
-        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
-        var firstError = errors[0];
-
-        var statusCode = firstError.Type switch
+        protected IActionResult Problem(List<Error> errors)
         {
-          ErrorType.Conflict => StatusCodes.Status409Conflict,
-          ErrorType.Validation => StatusCodes.Status400BadRequest,
-          ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
-          ErrorType.Forbidden => StatusCodes.Status403Forbidden,
-          ErrorType.NotFound => StatusCodes.Status404NotFound,
-          _ => 500
-        };
+            if (errors.Count is 0)
+            {
+                return Problem();
+            }
 
-        return Problem(
-          detail: firstError.Description,
-          statusCode: statusCode
-        );
-      }
+            if (errors.All(error => error.Type == ErrorType.Validation))
+            {
+                return ValidationProblem(errors);
+            }
+
+            HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+            return Problem(errors[0]);
+        }
+
+        private ObjectResult Problem(Error error)
+        {
+            var statusCode = error.Type switch
+            {
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+                ErrorType.Forbidden => StatusCodes.Status403Forbidden,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                _ => 500
+            };
+
+            return Problem(
+              detail: error.Description,
+              statusCode: statusCode
+            );
+        }
+
+        private ActionResult ValidationProblem(List<Error> errors)
+        {
+            var modelStateDictionary = new ModelStateDictionary();
+            foreach (var error in errors)
+            {
+                modelStateDictionary.AddModelError(error.Code, error.Description);
+            }
+            return ValidationProblem(modelStateDictionary);
+        }
     }
 }
